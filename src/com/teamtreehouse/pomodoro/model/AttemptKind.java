7ce9bcf01b0df3def0ad77df364713509a1e3d94
pownallk <pownallk@gmail.com>
2016-11-23 11:51:43 -0600

    initial commit: building Model class, exploring Enums

---
+++ src/com/teamtreehouse/pomodoro/model/AttemptKind.java
@@ -0,0 +1,17 @@
+package com.teamtreehouse.pomodoro.model;
+
+public enum AttemptKind {
+
+    FOCUS(25 * 60),
+    BREAK(5 * 60);
+
+    private int mTotalSeconds;
+
+    AttemptKind(int totalSeconds) {
+        mTotalSeconds = totalSeconds;
+    }
+
+    public int getTotalSeconds() {
+        return mTotalSeconds;
+    }
+}
